87d6b96cfcdb55192acfe6eed9f1b0d8ab7ecc73
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2019-12-19 18:47:46 -0300

    Remove Powerlevel10k package installation

--- utils/terminal.sh
+++ utils/terminal.sh
@@ -2,16 +2,6 @@
 
 install_omzsh(){
     printf "${BOOTSTRAPPER}BOOTSTRAPPER${OFF} - ${BOLD}Installing Oh My Zsh ${OFF} \n"
-    # curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh; zsh
-    printf "\n"
-}
-
-install_powerlevel10k(){
-    printf "${BOOTSTRAPPER}BOOTSTRAPPER${OFF} - ${BOLD}Installing Powerlevel10k ${OFF} \n"
-    printf "\n"
-}
-
-set_zshrc_profile(){
-    printf "${BOOTSTRAPPER}BOOTSTRAPPER${OFF} - ${BOLD}Setting .zshrc profile ${OFF} \n"
+    curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh; zsh
     printf "\n"
 }
